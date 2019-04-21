36eded883c67a39b641cb856f33b034a906c90b0
rayz2007 <rayz2007@uw.edu>
2019-04-20 17:04:06 -0700

    solved backwash issue

--- Percolation.java
+++ Percolation.java
@@ -8,6 +8,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     public WeightedQuickUnionUF uf;
+    public WeightedQuickUnionUF back;
     public boolean[] isClosed;
     public boolean[] isFull;
     public int openAmount;
@@ -20,6 +21,7 @@ public class Percolation {
         }
         this.n = n;
         this.uf = new WeightedQuickUnionUF(n * n + 2);
+        this.back = new WeightedQuickUnionUF(n * n + 2);
         this.isClosed = new boolean[n * n + 2];
         this.isFull = new boolean[n * n + 2];
         this.openAmount = 0;
@@ -37,10 +39,34 @@ public class Percolation {
             isClosed[ind] = false;
             if (row - 1 > 0 && isOpen(row - 1, col)) {
                 uf.union(ind, index(row - 1, col));
+                back.union(ind, index(row - 1, col));
             }
             if (row < n && isOpen(row + 1, col)) {
-                uf.union();
+                uf.union(ind, index(row + 1, col));
+                back.union(ind, index(row + 1, col));
             }
+            if (col > 1 && isOpen(row, col - 1)) {
+                uf.union(ind, index(row, col - 1));
+                back.union(ind, index(row, col - 1));
+            }
+            if (col < n && isOpen(row, col + 1)) {
+                uf.union(ind, index(row, col + 1));
+                back.union(ind, index(row, col + 1));
+            }
+            if (row == 1) {
+                uf.union(0, ind);
+                back.union(ind, 0);
+                isFull[ind] = true;
+            }
+            if (row == n) {
+                back.union(ind, n * n + 1);
+                if (isFull[ind]) {
+                    isFull[n * n + 1] = true;
+                }
+            }
+            openAmount++;
+            isClosed[ind] = false;
+
         }
     }
 
@@ -60,8 +86,8 @@ public class Percolation {
         if (row <= 0 || row > n || col <= 0 || col > n) {
             throw new IllegalArgumentException();
         }
-        int index = (row - 1) * n + col;
-        return uf.connected(0, index);
+        int ind = (row - 1) * n + col;
+        return uf.connected(0, ind);
 
     }
 
@@ -70,7 +96,27 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return (uf.connected(0, n * n + 1));
+        return back.connected(0, n * n + 1);
     }
+
+   /* private void print(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Percolation p = new Percolation(3);
+            p.open(1, 1);
+            p.open(2, 1);
+            p.open(3, 1);
+            System.out.println((p.isOpen(1, 1)));
+            System.out.println((p.isFull(1, 1)));
+            System.out.println((p.percolates()));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    } */
+
 }
 
